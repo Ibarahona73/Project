8b685c3879509cff52f186ee50c4c4b72885d478
Ivan Barahona <igyt2015@gmail.com>
2024-02-09 14:22:02 -0600

    Add project files.

---
+++ Views/Login.xaml.cs
@@ -0,0 +1,24 @@
+namespace Project.Views;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void IngresarBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OlvidarPassBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RegisterBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
